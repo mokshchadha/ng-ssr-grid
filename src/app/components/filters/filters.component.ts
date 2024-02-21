bf37519309c8d80f2f704fd07a4926397e3d78b2
moksh chadha <mokshchadha@mokshs-MacBook-Air.local>
2024-02-21 21:55:23 +0530

    added material theme and debounced search on the form

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,14 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css',
 })
-export class FiltersComponent implements OnInit {
-  columns = [{ label: 'order No', field: 'order', sorting: 'ASC' }];
+export class FiltersComponent implements OnInit, OnDestroy {
+  signUpForm: FormGroup = new FormGroup({});
+  columns = [
+    {
+      label: 'Order No',
+      field: 'order',
+      sorting: 'ASC',
+      type: 'text',
+      defaultValue: '',
+    },
+    {
+      label: 'Quantity',
+      field: 'quantity',
+      sorting: 'ASC',
+      type: 'number',
+      defaultValue: '',
+    },
+  ];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let controllers: any = {};
+    for (let c of this.columns) {
+      const fieldName = c.field;
+      controllers[fieldName] = new FormControl();
+    }
+
+    console.log({ controllers });
+    this.signUpForm = new FormGroup(controllers);
+
+    this.signUpForm.valueChanges.subscribe((e) => {
+      function search() {
+        console.log('Searching for:', e);
+      }
+      const debounceSearch = _.debounce(search, 1500);
+      debounceSearch();
+    });
+  }
+
+  ngOnDestroy(): void {}
 }
